405f756794bc6bb4d362be3747df67c7de074ff4
James Boynton <james.boynton0@gmail.com>
2019-07-31 00:48:57 -0400

    WIP

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -4,7 +4,7 @@ autoload -Uz vcs_info
 zstyle ':vcs_info:*' unstagedstr '*'
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' actionformats '%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f'
-zstyle ':vcs_info:*' formats ' %F{5}%F{2}‹%b›%F{5} %F{2}%c%F{3}%u%f'
+zstyle ':vcs_info:*' formats ' %F{5}%F{2}‹%b›%F{5} %F{2}%c%F{3}%u%f '
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 zstyle ':vcs_info:*' enable git
 
@@ -16,6 +16,6 @@ zstyle ':vcs_info:*' enable git
 }
 
 precmd () { vcs_info }
-PROMPT='%F{3}%3~${vcs_info_msg_0_} %f$ '
+PROMPT='%F{3}%3~${vcs_info_msg_0_}%f$ '
 
 # vim:ft=zsh
